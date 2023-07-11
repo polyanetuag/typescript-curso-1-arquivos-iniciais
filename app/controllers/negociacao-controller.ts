7afe12eac06669f7e2223995e01697adf60792f5
Polyane Tuag <pollyportugues@hotmail.com>
2023-07-11 13:46:34 -0300

    feat: typing and converting the data

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -1,17 +1,24 @@
+import { Negociacao } from "../models/negociacao.js";
+
 export class NegociacaoController {
-    private inputData;
-    private inputQuantidade;
-    private inputValor;
+  private inputData: HTMLInputElement;
+  private inputQuantidade: HTMLInputElement;
+  private inputValor: HTMLInputElement;
 
-    constructor() {
-        this.inputData = document.querySelector('#data');
-        this.inputQuantidade = document.querySelector('#quantidade');
-        this.inputValor = document.querySelector('#valor');
-    }
+  constructor() {
+    this.inputData = document.querySelector("#data");
+    this.inputQuantidade = document.querySelector("#quantidade");
+    this.inputValor = document.querySelector("#valor");
+  }
 
-    adiciona() {
-        console.log(this.inputData);
-        console.log(this.inputQuantidade);
-        console.log(this.inputValor);
-    }
+  adiciona() {
+    // convertendo os tipos de dados
+    const exp = /-/g;
+    const date = new Date(this.inputData.value.replace(exp, ","));
+    const quantidade = parseInt(this.inputQuantidade.value);
+    const valor = parseFloat(this.inputValor.value);
+
+    const negociacao = new Negociacao(date, quantidade, valor);
+    console.log(negociacao);
+  }
 }
