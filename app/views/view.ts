aa8245d2a639da2fa3e6836837ee6b58a2c3e78f
Polyane Tuag <pollyportugues@hotmail.com>
2023-08-04 16:28:10 -0300

    feat: create reusable code

---
+++ app/views/view.ts
@@ -0,0 +1,15 @@
+export class View<T> {
+    protected elemento: HTMLElement;
+
+    constructor(seletor: string) {
+        this.elemento = document.querySelector(seletor);
+    }
+    update(model: T): void {
+        const template = this.template(model)
+        this.elemento.innerHTML = template;
+      }
+
+    template(model: T): string {
+        throw new Error("Method not implemented.");
+    }
+} 
