a0ced052f66a70c7ec2c351286667295da512b1a
Polyane Tuag <pollyportugues@hotmail.com>
2023-08-05 14:29:06 -0300

    fix: transforming View in abstract

--- app/views/view.ts
+++ app/views/view.ts
@@ -1,4 +1,4 @@
-export class View<T> {
+export abstract class View<T> {
     protected elemento: HTMLElement;
 
     constructor(seletor: string) {
@@ -9,7 +9,5 @@ export class View<T> {
         this.elemento.innerHTML = template;
       }
 
-    template(model: T): string {
-        throw new Error("Method not implemented.");
-    }
+    abstract template(model: T): string;
 } 
