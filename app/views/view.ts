0fe63f8a67ff19f8e6562d4d55dabf63d47f6dd9
Polyane Tuag <pollyportugues@hotmail.com>
2023-08-12 10:54:34 -0300

    fix: optional changing

--- app/views/view.ts
+++ app/views/view.ts
@@ -2,8 +2,12 @@ export abstract class View<T> {
     protected elemento: HTMLElement;
     private escapar = false;
 
-    constructor(seletor: string, escapar: boolean) {
+    constructor(seletor: string, escapar?: boolean) {
         this.elemento = document.querySelector(seletor);
+
+        if(escapar) {
+            this.escapar = escapar;
+        }
     }
     public update(model: T): void {
         let template = this.template(model)
