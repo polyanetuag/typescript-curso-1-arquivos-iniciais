0e6c0267c5e91ba79db4f0acfcb6a7908600365a
Polyane Tuag <pollyportugues@hotmail.com>
2023-08-15 18:48:37 -0300

    fix: validate selector

--- app/views/view.ts
+++ app/views/view.ts
@@ -1,23 +1,27 @@
 export abstract class View<T> {
-    protected elemento: HTMLElement;
-    private escapar = false;
+  protected elemento: HTMLElement;
+  private escapar = false;
 
-    constructor(seletor: string, escapar?: boolean) {
-        this.elemento = document.querySelector(seletor);
-
-        if(escapar) {
-            this.escapar = escapar;
-        }
+  constructor(seletor: string, escapar?: boolean) {
+    const elemento = document.querySelector(seletor);
+    if (elemento) {
+      this.elemento = elemento as HTMLElement;
+    } else {
+      throw Error(`Seletor ${seletor} não existe no DOM. Verifique`);
     }
-    public update(model: T): void {
-        let template = this.template(model)
+    if (escapar) {
+      this.escapar = escapar;
+    }
+  }
+  public update(model: T): void {
+    let template = this.template(model);
 
-        if(this.escapar) {
-            template = template.replace(/<script>[\s\S]*?<\/script>/, '')
-        }
+    if (this.escapar) {
+      template = template.replace(/<script>[\s\S]*?<\/script>/, "");
+    }
 
-        this.elemento.innerHTML = template;
-      }
+    this.elemento.innerHTML = template;
+  }
 
-    protected abstract template(model: T): string;
-} 
+  protected abstract template(model: T): string;
+}
