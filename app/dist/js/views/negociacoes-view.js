029e6c32e031bb58bb4613bf91704f5ca7211840
Polyane Tuag <pollyportugues@hotmail.com>
2023-08-21 17:02:08 -0300

    feat: rules application

--- app/dist/js/views/negociacoes-view.js
+++ app/dist/js/views/negociacoes-view.js
@@ -1,8 +1,6 @@
-export class NegociacoesView {
-    constructor(seletor) {
-        this.elemento = document.querySelector(seletor);
-    }
-    template() {
+import { View } from "./view.js";
+export class NegociacoesView extends View {
+    template(model) {
         return `
             <table class="table table-hover table-bordered">
                 <thead>
@@ -14,14 +12,26 @@ export class NegociacoesView {
                 </thead>
                 
                 <tbody>
+                ${model
+            .lista()
+            .map((negociacao) => {
+            return `
+                      <tr>
+                        <td>${this.formatar(negociacao.data)}</td>
+                        <td>${negociacao.quantidade}</td>
+                        <td>${negociacao.valor}</td>
+                      </tr>
+                  `;
+        })
+            .join("")}
+
                 </tbody>
-                
                 <tfoot>
                 </tfoot>
             </table>               
         `;
     }
-    update() {
-        this.elemento.innerHTML = this.template();
+    formatar(data) {
+        return new Intl.DateTimeFormat().format(data);
     }
 }
