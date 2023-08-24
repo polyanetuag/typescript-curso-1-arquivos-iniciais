aaceca5cd99a1060320b3c02f87abcf4915945b5
Polyane Tuag <pollyportugues@hotmail.com>
2023-08-24 18:42:18 -0300

    feat: structure decorator

---
+++ app/dist/js/decorators/logar-tempo-de-execucao.js
@@ -0,0 +1,13 @@
+export function logarTempoDeExecucao() {
+    return function (target, propertyKey, descriptor) {
+        const metodoOriginal = descriptor.value;
+        descriptor.value = function () {
+            const t1 = performance.now();
+            const retorno = metodoOriginal();
+            const t2 = performance.now();
+            console.log(`${propertyKey}: O tempo de execução foi de ${(t2 - t1) / 1000} segundos`);
+            retorno;
+        };
+        return descriptor;
+    };
+}
--- app/src/decorators/logar-tempo-de-execucao.ts
+++ app/src/decorators/logar-tempo-de-execucao.ts
@@ -4,6 +4,16 @@ export function logarTempoDeExecucao() {
         propertyKey: string,
         descriptor: PropertyDescriptor
     ) {
+        const metodoOriginal = descriptor.value
+        descriptor.value = function() {
+            const t1 = performance.now()
+            const retorno = metodoOriginal()
+            const t2 = performance.now()
+            console.log(`${propertyKey}: O tempo de execução foi de ${(t2 - t1)/1000} segundos`)
+            retorno
+        } 
+
+
         return descriptor
     }
 }
