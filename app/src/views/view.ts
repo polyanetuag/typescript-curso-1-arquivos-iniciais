5cde0947106b26fa09d0a70054a0529b6ec05a11
Polyane Tuag <pollyportugues@hotmail.com>
2023-08-28 18:28:04 -0300

    feat: params

--- app/src/views/view.ts
+++ app/src/views/view.ts
@@ -15,7 +15,7 @@ export abstract class View<T> {
       this.escapar = escapar;
     }
   }
-  @logarTempoDeExecucao()
+  @logarTempoDeExecucao(true)
   public update(model: T): void {
     let template = this.template(model);
 
