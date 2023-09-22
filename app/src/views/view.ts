0bee54bdfdd7f717b4d230bee9c5813cb5b4ed3c
Polyane Tuag <pollyportugues@hotmail.com>
2023-09-22 18:37:05 -0300

    fix: clean code

--- app/src/views/view.ts
+++ app/src/views/view.ts
@@ -1,6 +1,3 @@
-import { inspect } from "../decorators/inspect.js";
-import { logarTempoDeExecucao } from "../decorators/logar-tempo-de-execucao.js";
-
 export abstract class View<T> {
   protected elemento: HTMLElement;
 
@@ -12,8 +9,7 @@ export abstract class View<T> {
       throw Error(`Seletor ${seletor} não existe no DOM. Verifique`);
     }
   }
-  @inspect()
-  @logarTempoDeExecucao(true)
+
   public update(model: T): void {
     let template = this.template(model);
 
