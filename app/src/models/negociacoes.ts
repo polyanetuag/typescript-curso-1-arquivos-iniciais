d9bfb9aa9690c52273d25e67e4ad2ce2abda6503
Polyane Tuag <pollyportugues@hotmail.com>
2023-09-24 10:53:55 -0300

    fix: implements model

--- app/src/models/negociacoes.ts
+++ app/src/models/negociacoes.ts
@@ -1,18 +1,22 @@
-import { Imprimivel } from "../utils/imprimivel.js";
-import { Negociacao } from "./negociacao.js";
+import { Modelo } from '../interfaces/modelo.js';
+import { Negociacao } from './negociacao.js';
+export class Negociacoes implements Modelo<Negociacoes> {
 
-export class Negociacoes implements Imprimivel {
-    private negociacoes: Negociacao[] = []
+    private negociacoes: Negociacao[] = [];
 
     public adiciona(negociacao: Negociacao) {
-        this.negociacoes.push(negociacao)
+        this.negociacoes.push(negociacao);
     }
 
     public lista(): readonly Negociacao[] {
-        return this.negociacoes
+        return this.negociacoes;
     }
 
     public paraTexto(): string {
-        return JSON.stringify(this.negociacoes, null,2)
+        return JSON.stringify(this.negociacoes, null, 2);
+    }
+
+    public ehIgual(negociacoes: Negociacoes): boolean {
+        return JSON.stringify(this.negociacoes) === JSON.stringify(negociacoes.lista());
     }
 }
